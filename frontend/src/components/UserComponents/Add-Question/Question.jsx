2df3b4eab7b54932d962088c64f766c42d4d4b6b
Anurag M K <109131413+Anurag-M-K@users.noreply.github.com>
2023-02-24 19:37:31 +0530

    deploying

--- frontend/src/components/UserComponents/Add-Question/Question.jsx
+++ frontend/src/components/UserComponents/Add-Question/Question.jsx
@@ -6,9 +6,9 @@ import "./Question.css";
 import { TagsInput } from "react-tag-input-component";
 import { useSelector } from "react-redux";
 import Editor from "react-quill/lib/toolbar";
-import { ToastContainer, toast } from 'react-toastify';
+// import { ToastContainer, toast } from 'react-toastify';
 import { addQuestion } from "../../../helper/userQuestionHelper";
-import 'react-toastify/dist/ReactToastify.css';
+// import 'react-toastify/dist/ReactToastify.css';
 
 function Question() {
 
@@ -20,9 +20,9 @@ function Question() {
   const navigate = useNavigate();
   const { tokenData } = useSelector((state)=>state.user)
 
-  const notify = () =>  toast.success("Question added successfully!",{
-    position:"top-right"
-  });
+  // const notify = () =>  toast.success("Question added successfully!",{
+  //   position:"top-right"
+  // });
   
 
 
@@ -150,7 +150,7 @@ function Question() {
                   onChange={setTag}
                   name="fruits"
                   placeHolder="enter add new tag" />
-                    <ToastContainer/>
+                    {/* <ToastContainer/> */}
                 </div>
               </div>
             </div>
--- frontend/src/config/axiosInstance.js
+++ frontend/src/config/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:80",
+    baseURL: "https://bugfixer-97dk.onrender.com",
     
 });
 
